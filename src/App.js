426086852197d2578a82a5a70bdf872332017baf
ankush-anonymous <canankushdoit@gmail.com>
2024-01-27 20:22:16 +0530

    aboutUs url changed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/about" element={<AboutUsPage />} />
+          <Route path="/" element={<AboutUsPage />} />
 
           {/* <Route
             path="/admin/home"
